877c34207903b0dfda4a5fdf1c85683f0997972e
S6758 <109202341+S6758@users.noreply.github.com>
2022-07-14 14:02:14 +0545

    Mywebsite

---
+++ ins.php
@@ -0,0 +1,60 @@
+<?php
+if (isset($_POST['submit'])) {
+    if (isset($_POST['username']) &&  isset($_POST['email']) &&
+        isset($_POST['phoneCode']) && isset($_POST['phone'])) {
+        
+        $username = $_POST['username'];
+        $email = $_POST['email'];
+        $phoneCode = $_POST['phoneCode'];
+        $phone = $_POST['phone'];
+
+        $host = "localhost";
+        $dbUsername = "root";
+        $dbPassword = "";
+        $dbName = "register";
+
+        $conn = new mysqli($host, $dbUsername, $dbPassword, $dbName);
+
+        if ($conn->connect_error) {
+            die('Could not connect to the database.');
+        }
+        else {
+            $Select = "SELECT email FROM register WHERE email = ? LIMIT 1";
+            $Insert = "INSERT INTO register(username, email, phoneCode, phone) values(?, ?, ?, ?)";
+
+            $stmt = $conn->prepare($Select);
+            $stmt->bind_param("s", $email);
+            $stmt->execute();
+            $stmt->bind_result($resultEmail);
+            $stmt->store_result();
+            $stmt->fetch();
+            $rnum = $stmt->num_rows;
+
+            if ($rnum == 0) {
+                $stmt->close();
+
+                $stmt = $conn->prepare($Insert);
+                $stmt->bind_param("ssii",$username, $email, $phoneCode, $phone);
+                if ($stmt->execute()) {
+                    echo "New record inserted sucessfully."; echo'<a href="index.html">Click Ok</a>';
+                }
+                else {
+                    echo $stmt->error;
+                }
+            }
+            else {
+                echo "Someone already registers using this email.";
+            }
+            $stmt->close();
+            $conn->close();
+        }
+    }
+    else {
+        echo "All field are required.";
+        die();
+    }
+}
+else {
+    echo "Submit button is not set";
+}
+?>
